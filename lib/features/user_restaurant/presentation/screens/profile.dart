dcd78f59e43aac8228b250bdd03db874b0b093c0
Rinku <saashish7070@gmail.com>
2025-10-23 19:35:41 +0545

    Setup for the user-restaurant is done perfectly

---
+++ lib/features/user_restaurant/presentation/screens/profile.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rms/shared/widgets/appbar.dart';
+import 'package:rms/shared/widgets/profile/restaurant_management.dart';
+import 'package:rms/shared/widgets/profile/setting_list.dart';
+import 'package:rms/shared/widgets/profile/support.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const Color brandOrange = Color(0xFFF7621B);
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(brandOrange: brandOrange, name: "Profile"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        'assets/images/profile.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "Sita Khadka",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const Text(
+                      "khadkasita@gmail.com",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              SettingList(),
+              RestaurantManagementList(),
+              SupportList()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
